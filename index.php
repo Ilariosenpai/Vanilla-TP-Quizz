ab16983c4b68996a7a657c4b8c67ce23515f87c5
alexis-moreno-lopez <alexis.morenolopezpro@gmail.com>
2023-12-21 12:56:52 +0100

    header-footer

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include_once('../partials/headerNone.php');
+include_once('./partials/headerNone.php');
 
 // require_once('../database.connect.php');
 
@@ -8,11 +8,11 @@ include_once('../partials/headerNone.php');
 
 ?>
 
-  <div class="d-flex justify-content-center">
-   <div></div>
+  <div class="d-flex justify-content-center ">
+
   <form action="../procces/traitementIndex.php" method="post">
 
-  <div class="mb-3">
+  <div class="mb-3 d-flex align-item-center">
     <label for="exampleInputEmail1" class="form-label">Pseudo</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     
@@ -20,9 +20,9 @@ include_once('../partials/headerNone.php');
 
   <button type="submit" class="btn btn-primary">Go</button>
 </form>
-
+</div>
 </main>
 
 <?php
-include_once('../partials/footer.php')
+include_once('./partials/footer.php')
 ?>
--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,11 @@
 
-
+<footer class="bg-danger">
+<div class="d-flex justify-content-around">
+<h2>Ilan</h2>
+<h2>Turan</h2>
+<h2>Alexis</h2>
+</div>
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
