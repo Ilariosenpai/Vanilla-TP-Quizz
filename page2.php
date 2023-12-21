607a165a11082dc9e017586b36ceb20b1b4b66b6
Tkumbasar <turan-kumbasar@live.fr>
2023-12-21 16:36:36 +0100

    page-2

--- page2.php
+++ page2.php
@@ -8,7 +8,7 @@
     <title>Salameche-page2</title>
 </head>
 
-
+<?php include_once('./partials/header.php') ?>
 
 <body>
 
@@ -32,9 +32,8 @@
 
 </form>
 
-
-
-
+<?php include_once('./partials/footer.php') ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </body>
 </html>
--- traitement-page2.php
+++ traitement-page2.php
@@ -1,14 +1,34 @@
-<?php 
+<?php
 require_once('./bdd/connextion.php');
 
-if (isset($_POST['name']) && !empty($_POST['name']) && 
-    isset($_POST['start']) && !empty($_POST['start'])) {
 
-}else {
+
+if (isset($_POST['name']) && !empty($_POST['name'])) {
+
+
+    $request = $database->prepare("SELECT * FROM `user` WHERE pseudo =:pseudo");
+    $request->execute([
+        ':pseudo' => $_POST['name']
+    ]);
+    // $pseudo = $requete->fetchall(PDO::FETCH_ASSOC);
+
+    if ($_POST['name'] === $pseudo) { 
+    echo " euhh ma tata a une couille";
+    }
+
+    if ($pseudo) {
+
+        $request = $database->prepare("INSERT INTO `user` (pseudo) VALUES (:pseudo)");
+        $request->execute([
+            'pseudo' => $_POST["name"]
+
+        ]);
+    }
+
+
+} else {
+
     echo 'nous sommes en guerre';
+
 }
 
-
-
-
-?>
