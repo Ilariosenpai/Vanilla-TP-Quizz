4b83ae0dd2affa952c6bffd85b92152bdba0e8f8
Tkumbasar <turan-kumbasar@live.fr>
2023-12-22 16:47:36 +0100

    fin-page2

--- page3.php
+++ page3.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once('./partials/header.php')
 
 ?>
 
@@ -15,12 +15,10 @@
 
 
 
-<h2 class="d-flex justify-content-center">Votre score est de <?php $_POST['score']  ?> points </h2>
-
-
-
-
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
+<?php
+include_once('./partials/footer.php')
+
+?>
